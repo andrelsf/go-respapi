54f67f9f0e5fcb07db9c3bd5f63c9362e30f2436
Andre Ferreira <andre.souza@sensedia.com>
2022-03-05 11:51:22 -0300

    Added GetAccountForUpdate and AddAccountBalance

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,6 +9,16 @@ RETURNING *;
 SELECT * FROM accounts 
 WHERE id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT 
+  * 
+FROM 
+  accounts 
+WHERE 
+  id = $1 
+LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id 
@@ -21,6 +31,16 @@ SET
   balance = $2
 WHERE 
   id = $1
+RETURNING 
+  *;
+
+-- name: AddAccountBalance :one
+UPDATE 
+  accounts
+SET 
+  balance = balance + sqlc.arg(amount)
+WHERE 
+  id = sqlc.arg(id)
 RETURNING 
   *;
 
